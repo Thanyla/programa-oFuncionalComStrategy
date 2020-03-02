5dbaf6e43bb849750e74d3bdd6cf1147b8bf63d1
Thanyla <thanylas@gmail.com>
2020-03-02 08:53:17 -0300

    Implemetnação da classe ProcessarBoletos

--- mavenproject3/src/main/java/com/mycompany/mavenproject3/ProcessarBoletos.java
+++ mavenproject3/src/main/java/com/mycompany/mavenproject3/ProcessarBoletos.java
@@ -1,14 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.mycompany.mavenproject3;
+
+import java.util.List;
+import java.util.function.Function;
 
 /**
  *
- * @author aluno
+ * @author Thanyla
  */
 public class ProcessarBoletos {
+    //Criação de função e utilização de genericos para expecificar o tipo da função
+    //Primeiro generico especifica o tipo de entrada e o segundo o retorno 
+    private Function<String, List<Boleto>> lerArquivo;
     
+    public void processar(String nomeArquivo){
+        System.out.println("Iniciando processamento do arquivo");
+        List<Boleto> boletos = lerArquivo.apply(nomeArquivo);
+        for (Boleto boleto : boletos) {
+            System.out.println(boleto);
+        }
+    }
 }
